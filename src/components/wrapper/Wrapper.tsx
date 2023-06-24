2385ce7e8b9ab8620b9f448f68586aeb6e42b6c1
aimner <aimner510@gmail.com>
2023-06-24 22:38:53 +0300

    feat: add wraper and nav component

--- src/components/wrapper/Wrapper.tsx
+++ src/components/wrapper/Wrapper.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { Nav } from "../nav";
 
-import classes from './Wrapper.module.scss'
+import classes from "./Wrapper.module.scss";
+
 
 export const Wrapper = () => {
   return (
-    <div><Outlet /></div>
-  )
-}
+    <div className={classes.app}>
+      <Nav />
+      <Outlet />
+    </div>
+  );
+};
